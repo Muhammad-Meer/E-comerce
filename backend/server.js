aa64c992e146b0e561eae0dcf8ebff3ccd22dd89
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-07-02 12:24:33 +0500

    code

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
 const app = require('./src/app');
 const express = require('exress')
+const dotnev = require('dotenv')
+const cors = require('cors')
+
+
+dotnev.config();
+
+
+
+app.use('/', (req , res ) => {
+  res.send("hello")
+})
 
 const PORT = process.env.PORT || 5000;
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+console.
 const App = () => {
   return (
     <>
