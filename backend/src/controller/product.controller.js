4c68566f94da255a35fb02c0974f596daf35e5e6
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-07-04 10:42:40 +0500

    noeupdate

---
+++ backend/src/controller/product.controller.js
@@ -0,0 +1,22 @@
+const getproducts = async (req, res) => {
+};
+
+const createproduct = async (req, res) => {
+};
+
+const getproductsByid = async (req, res) => {
+};
+
+const updateprodeuct = async (req, res) => {
+};
+
+const deleteproduct = async (req, res) => {
+};
+
+module.exports = {
+  getproducts,
+  createproduct,
+  getproductsByid,
+  updateprodeuct,
+  deleteproduct
+};
--- backend/src/routers/productRoutes.js
+++ backend/src/routers/productRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const  protected = require('../middleware/auth.middleware')
 const  admin = require('../middleware/admin.middleware')
+const {getproducts ,createproduct , getproductsByid , updateprodeuct , deleteproduct } = require('../controller/product.controller')
 
 
 const router = express.Router()
 router.route('/').get(getproducts).post(protected , admin , createproduct)
-router.route('/').get(getproductsByid).put(protected , admin , updateprodeuct).delete(protected , admin , deleteproduct)
+
+router.route('/:id').get(getproductsByid).put(protected , admin , updateprodeuct).delete(protected, admin , deleteproduct)
+
+
+
 
 
 
