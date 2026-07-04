158cc26a6c9eaee11664561c70e0c79a1b65649b
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-07-04 10:19:35 +0500

    n

--- backend/src/routers/productRoutes.js
+++ backend/src/routers/productRoutes.js
@@ -5,6 +5,7 @@ const  admin = require('../middleware/admin.middleware')
 
 const router = express.Router()
 router.route('/').get(getproducts).post(protected , admin , createproduct)
+router.route('/').get(getproductsByid).put(protected , admin , updateprodeuct).delete(protected , admin , deleteproduct)
 
 
 
