8998e72c0568de027547f1e5369c960f3f06cef7
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-07-05 17:19:56 +0500

    newcodeupdate

---
+++ backend/src/models/order.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  items: [
+    {
+      itemsId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+      quantity: { type: Number, required: true, min: 1 }
+    }
+  ],
+
+  totalAmount: { type: Number, required: true },
+  address: {
+    fullName: { type: String, required: true },
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true }
+  },
+
+  paymentId: { type: String, required: true },
+  status: { type: String, enum: ['pending', 'shipped', 'delivered'], default: 'pending' },
+
+}, { timestamps: true });
+
+module.exports = mongoose.exports = mongoose.model('Order', orderSchema);
