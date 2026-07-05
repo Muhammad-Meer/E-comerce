b8db363238dae73c468d3a7aefb6f4f0941d0437
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-07-05 16:03:49 +0500

    newcodee

--- backend/src/routers/orderRoutes.js
+++ backend/src/routers/orderRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { protect } = require('../middleware/auth.middleware');
+const { protectd } = require('../middleware/auth.middleware');
 const { admin } = require('../middleware/admin.middleware');
 const { createOrder, getOrders, getOrderById, updateOrderStatus } = require("../controller/order.controller");
 
 const router = express.Router();
 
 router.route('/').post(protect, createOrder).get(protect, admin, getOrders);
+router.route('/myorders').get(protectd, getOrderById )
 router.route('/:id').get(protect, getOrderById).put(protect, admin, updateOrderStatus);
 
 module.exports = router;
